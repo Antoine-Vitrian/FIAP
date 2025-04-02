15136021d3ad67999c2a82a934814709e0553d50
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-02 09:35:38 -0300

    javascript

--- Web Development/Aula01.js
+++ Web Development/Aula01.js
@@ -1,12 +1,12 @@
-// const ola = document.getElementById("hello")
+const ola = document.getElementById("hello")
 
-// function hello(){
-//     ola.textContent = "Hello World!"
-// }
+function hello(){
+    ola.textContent = "Hello World!"
+}
 
 // console.log("Olá Javascript!")
 // document.querySelector("h1").style.color = "blue"
 
-console.log("Olá! Meu amigo Node.js")
-const soma = 5 + 3
-console.log("Resultado da soma igual a", soma)
+// console.log("Olá! Meu amigo Node.js")
+// const soma = 5 + 3
+// console.log("Resultado da soma igual a", soma)
