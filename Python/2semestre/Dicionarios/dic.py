52474ef3268ba4f9654141a4a30a8f31c89fd9b9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-11 11:36:15 -0300

    m

---
+++ Python/2semestre/Dicionarios/dic.py
@@ -0,0 +1,106 @@
+'''dic = {'chave' : 'valor'}
+print(dic['chave'])
+dic['nova chave'] = 'novo valor'
+print(dic)
+dic['chave'] = '123'
+print(dic)
+import random
+
+saudacoes = {
+    'oi':['Olá','SALVE','iae','BÃO uai'],
+    'tchau':['flw','tchau migo','bjsssss diva','até mais']
+}
+while True:
+    resposta = input("Diga oi ou tchau: ")
+    print(random.choice(saudacoes[resposta]))
+
+if resposta == 'oi':
+    print('Olá')
+else:
+    print('flw')
+
+poligonos = {
+    '3' : 'Triângulo',
+    '4' : 'Quadrado',
+    '5' : 'Pentágono',
+    '6' : 'Hexágono'
+}
+resposta = input("Diga a qtd de lados do seu polígono: ")
+print(f"Você escolheu um {poligonos[resposta]}")
+
+emojis = {
+    ':)' : '😊',
+    ':(' : '😒',
+    '<3' : '❤️',
+    ":'(" : '😢'
+}
+texto = 'eu amo python <3'
+for key in emojis.keys():
+    texto = texto.replace(key,emojis[key])
+print(texto)
+
+numeros = {
+    'zero' : '0',
+    'um' : '1',
+    'dois' : '2',
+    'tres' : '3',
+    'quatro' : '4',
+    'cinco' : '5',
+    'seis' : '6',
+    'sete' : '7',
+    'oito' : '8',
+    'nove' : '9'
+}
+telefone = input("Diga seu telefone: ")
+for key in numeros.keys():
+    telefone = telefone.replace(key,numeros[key])
+print(telefone)
+telefone = telefone.replace(' ','')
+print(f"O número de telefone é {telefone}")
+
+dic = {
+    'nome' : ['danilo','Bernardo','Matheus'],
+    'idade' : [20,18,19]
+}
+for i in range(len(dic['nome'])):
+    for key in dic.keys():
+        print(f"{key} : {dic[key][i]}")
+    print()
+'''
+
+
+
+'''frase = "A aranha arranha a rã. A rã arranha a aranha. Nem a aranha arranha a rã. Nem a rã arranha a aranha."
+print(frase)
+frase = frase.lower()
+print(frase)
+frase = frase.replace('.','')
+print(frase)
+palavras = frase.split(' ')
+print(palavras)
+contador = {}
+for palavra in palavras:
+    if palavra not in contador.keys():
+        contador[palavra] = 1
+    else:
+        contador[palavra] += 1
+    print(contador)'''
+
+# ex 02 a 06
+carros = {
+    "nomes": ["celta", "up", "kombi", "uno"],
+    "portas": [4,6,2,4],
+    "preços": [1000,200,300,100],
+    "ano": [2014,2018,1970,2005]
+}
+
+maior = 0
+for i in range(len(carros["preços"])):
+    if carros["preços"][i] < maior:
+        maior = i
+    else:
+        pass
+print(f"Modelo: {carros['nomes'][maior]}\n"
+      f"Preço: {carros['preços'][maior]}\n"
+      f"Modelo: {carros['portas'][maior]}\n"
+      f"Modelo: {carros['ano'][maior]}")
